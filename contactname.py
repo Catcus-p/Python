a7b9bd33976215709e785b3d3c1d0ab3b863d98b
Catcus-p <chhetribarsha537@gmail.com>
2026-04-09 16:50:07 +0545

    contactname

---
+++ contactname.py
@@ -0,0 +1,57 @@
+contacts = {}
+
+def add_contact():
+    name = input("Enter name: ")
+    phone = input("Enter phone: ")
+    email = input("Enter email: ")
+
+    contacts[name] = {"phone": phone, "email": email}
+    print("Contact added successfully!")
+
+def view_contacts():
+    if not contacts:
+        print("No contacts found.")
+    else:
+        for name, info in contacts.items():
+            print(f"Name: {name}")
+            print(f"Phone: {info['phone']}")
+            print(f"Email: {info['email']}")
+            print("-------------------")
+
+def search_contact():
+    name = input("Enter name to search: ")
+    if name in contacts:
+        print(contacts[name])
+    else:
+        print("Contact not found")
+
+def delete_contact():
+    name = input("Enter name to delete: ")
+    if name in contacts:
+        del contacts[name]
+        print("Contact deleted")
+    else:
+        print("Contact not found")
+
+while True:
+    print("\nContact Book")
+    print("1. Add Contact")
+    print("2. View Contacts")
+    print("3. Search Contact")
+    print("4. Delete Contact")
+    print("5. Exit")
+
+    choice = input("Choose option: ")
+
+    if choice == "1":
+        add_contact()
+    elif choice == "2":
+        view_contacts()
+    elif choice == "3":
+        search_contact()
+    elif choice == "4":
+        delete_contact()
+    elif choice == "5":
+        break
+    else:
+        print("Invalid choice")
